10636c115f8e59f5be30b687c017cc7989273062
Irving E Rabel <rabel606@gmail.com>
2018-09-18 01:45:11 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,6 @@
 
 def display_rainbow(color_array)
   #here Iwill have to puts, using inst
-  colors
+  colors= ["red","orange","yellow","green","blue","indigo","violet"]
   puts "R: #{color_array[0]}, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
